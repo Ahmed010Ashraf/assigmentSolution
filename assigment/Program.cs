efdba66db108d8a8242daa9a00468cc78fc0578b
ahmed <aaboshady59@gmail.com>
2024-12-07 09:37:35 +0200

    Add project files.

---
+++ assigment/Program.cs
@@ -0,0 +1,117 @@
+﻿using System.Data.SqlTypes;
+
+namespace assigment
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            #region  1:Write a program that allows the user to enter a number then print it
+            //Console.WriteLine("enter a num : ");
+            //string x = Console.ReadLine();
+            //int y = int.Parse(x);
+            //Console.WriteLine(y);
+            #endregion
+
+
+            #region 2 :Write C# program that converts a string to an integer, but the string contains non-numeric characters. And mention what will happen 
+            //Console.WriteLine("enter a num : ");
+            //string x = Console.ReadLine();
+            //checked
+            //{
+            //    int y = int.Parse(x);
+            //    unchecked
+            //    {
+            //        Console.WriteLine(y);
+            //    }
+            //}
+            #endregion
+
+
+            #region 3 :Write C# program that Perform a simple arithmetic operation with floating-point numbers And mention what will happen
+            //float x = float.Parse(Console.ReadLine());
+            //float y = float.Parse(Console.ReadLine());
+            //Console.WriteLine(x+y);
+            //Console.WriteLine(x*y);
+            //Console.WriteLine(x-y);
+            //if (y > 0)
+            //{
+            //    Console.WriteLine(x/y);
+            //}
+            #endregion
+
+
+
+            #region  4:Write C# program that Extract a substring from a given string.
+
+
+            //string x = Console.ReadLine();
+            //string substring = x.Substring(1, 4);
+            //Console.WriteLine($"Extracted substring: {substring}");
+
+
+            #endregion
+
+
+
+            #region 5 : Write C# program that Assigning one value type variable to another and modifying the value of one variable and mention what will happen
+
+            //            string x = "ahmed";
+            //            string y = x;
+            //            x = "ali";
+            //            Console.WriteLine(y);
+            ////the variable y will not changed
+            #endregion
+
+
+            #region 6 : Write C# program that Assigning one reference type variable to another and modifying the object through one variable and mention what will happen
+
+            //            Person x = new Person();
+            //            x.x = 1;
+            //            Person y = new Person();
+            //            y = x;
+            //            x.x = 40;
+            //            Console.WriteLine(y.x);
+            ////the value of y will be changed depend on the value of x
+            #endregion
+
+
+
+
+            #region 7 : Write C# program that take two string variables and print them as one variable 
+            //string x = "ahmed";
+            //string y = "ashraf";
+            //string full = x + " " + y;
+            //Console.WriteLine(full);
+            #endregion
+
+
+
+
+
+            #region 8 : Which of the following statements is correct about the C#.NET code snippet given below?
+
+            // (b) the value 1 will assigend to (d)
+            #endregion
+
+
+
+
+            #region 9 : Which of the following is the correct output for the C# code given below?
+
+            //(d) 6  1
+            #endregion
+
+
+
+
+
+            #region 10 : What will be the output of the C# code given below?
+
+
+            //(d) 7  7
+            #endregion
+
+        }
+    }
+}
